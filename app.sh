ab2c2eca70ccece14ac059f9a7b3369a3b6641ac
ambujammu <ambujaweb123@gmail.com>
2024-07-20 22:10:53 +0530

    fixed

---
+++ app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update -y
+apt install nginx -y
+rm -r /var/www/html*
+git clone #!/bin/bash
+apt update -y
+apt install nginx -y
+rm -r /var/www/html/*
+git clone https://github.com/ravi2krishna/login-2418.git /var/www/html/
--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "koda_web_server" {
   instance_type = "t2.micro"
   key_name = "papu"
   subnet_id = aws_subnet.tcs_subnet.id
-  vpc_security_group_ids = aws_security_group.tcs_sg_web.id
+  vpc_security_group_ids = [aws_security_group.tcs_sg_web.id]
   user_data = file("app.sh)
 
   tags = {
