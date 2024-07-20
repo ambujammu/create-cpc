73ceae8e46e9f8c52bd2f20387189996eba0d805
ambujammu <ambujaweb123@gmail.com>
2024-07-20 15:16:22 +0530

    changes

--- virtualfile.tf
+++ virtualfile.tf
@@ -58,7 +58,7 @@ resource "aws_route_table" "koda-route1" {
   vpc_id = aws_vpc.koda_vpc.id
 
   route {
-    cidr_block = "0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.koda-gw.id
   }
 
