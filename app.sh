0a68a634b0c163a0ba838c9a0e307f7f37192620
ambujammu <ambujaweb123@gmail.com>
2024-07-20 22:14:00 +0530

    fixed

--- app.sh
+++ app.sh
@@ -2,8 +2,4 @@
 apt update -y
 apt install nginx -y
 rm -r /var/www/html*
-git clone #!/bin/bash
-apt update -y
-apt install nginx -y
-rm -r /var/www/html/*
 git clone https://github.com/ravi2krishna/login-2418.git /var/www/html/
