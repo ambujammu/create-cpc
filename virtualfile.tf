528306c314e8af4c8d3271bdac5752ba8bbe973c
ambujammu <ambujaweb123@gmail.com>
2024-07-20 13:38:21 +0530

    updated ip in subnet

--- virtualfile.tf
+++ virtualfile.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "koda_subnet" {
   vpc_id     = aws_vpc.koda_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-west-2a"
-  map_public_ip_on_lunch = "true"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "koda-subnet"
