f40429346ff88d0b2bd10562715173e6a1cde53c
ambujammu <ambujaweb123@gmail.com>
2024-07-20 17:24:55 +0530

    s

--- tc.tf
+++ tc.tf
@@ -1,100 +1,100 @@
 # Creating virtual networks
-resource "aws_vpc" "koda_vpc" {
+resource "aws_vpc" "tcs_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "koda-vpc"
+    Name = "tcs-vpc"
   }
 }
 
 # Creating Subnet for web server
-resource "aws_subnet" "koda_subnet" {
-  vpc_id     = aws_vpc.koda_vpc.id
+resource "aws_subnet" "tcs_subnet" {
+  vpc_id     = aws_vpc.tcs_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-west-2a"
   map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "koda-subnet"
+    Name = "tcs-subnet"
   }
 }
 
 # Creating Subnet for database server
-resource "aws_subnet" "koda_subnet_database" {
-  vpc_id     = aws_vpc.koda_vpc.id
+resource "aws_subnet" "tcs_subnet_database" {
+  vpc_id     = aws_vpc.tcs_vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-west-2a"
   map_public_ip_on_launch = "false"
 
   tags = {
-    Name = "koda-subnet-databse"
+    Name = "tcs-subnet-databse"
   }
 }
 
 # Creating Subnet for application server
-resource "aws_subnet" "koda_subnet_app" {
-  vpc_id     = aws_vpc.koda_vpc.id
+resource "aws_subnet" "tcs_subnet_app" {
+  vpc_id     = aws_vpc.tcs_vpc.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-west-2c"
   map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "koda-subnet-app"
+    Name = "tcs-subnet-app"
   }
 }
 
-# koda internet gateway
-resource "aws_internet_gateway" "koda-gw" {
-  vpc_id = aws_vpc.koda_vpc.id
+# tcs internet gateway
+resource "aws_internet_gateway" "tcs-gw" {
+  vpc_id = aws_vpc.tcs_vpc.id
 
   tags = {
-    Name = "koda-ig"
+    Name = "tcs-ig"
   }
 }
 
 #Creating Public Route Table
-resource "aws_route_table" "koda-route1" {
-  vpc_id = aws_vpc.koda_vpc.id
+resource "aws_route_table" "tcs-route1" {
+  vpc_id = aws_vpc.tcs_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.koda-gw.id
+    gateway_id = aws_internet_gateway.tcs-gw.id
   }
 
   tags = {
-    Name = "koda-route1"
+    Name = "tcs-route1"
   }
 }
 
 #Creating Private Route Table
-resource "aws_route_table" "koda-route-private" {
-  vpc_id = aws_vpc.koda_vpc.id
+resource "aws_route_table" "tcs-route-private" {
+  vpc_id = aws_vpc.tcs_vpc.id
 
   tags = {
-    Name = "koda-route1-private"
+    Name = "tcs-route1-private"
   }
 }
 
 # Map public subnet with public route
 resource "aws_route_table_association" "public-web-route" {
-  subnet_id      = aws_subnet.koda_subnet.id
-  route_table_id = aws_route_table.koda-route1.id
+  subnet_id      = aws_subnet.tcs_subnet.id
+  route_table_id = aws_route_table.tcs-route1.id
 }
 resource "aws_route_table_association" "public-app-route" {
-  subnet_id      = aws_subnet.koda_subnet_app.id
-  route_table_id = aws_route_table.koda-route1.id
+  subnet_id      = aws_subnet.tcs_subnet_app.id
+  route_table_id = aws_route_table.tcs-route1.id
 }
 
 # Map private subnet with private route
 resource "aws_route_table_association" "public-database-route" {
-  subnet_id      = aws_subnet.koda_subnet_database.id
-  route_table_id = aws_route_table.koda-route-private.id
+  subnet_id      = aws_subnet.tcs_subnet_database.id
+  route_table_id = aws_route_table.tcs-route-private.id
 }
 
 # Create NACLs
 resource "aws_network_acl" "web_nacl" {
-  vpc_id = aws_vpc.koda_vpc.id
+  vpc_id = aws_vpc.tcs_vpc.id
 
   egress {
     protocol   = "tcp"
@@ -115,19 +115,19 @@ resource "aws_network_acl" "web_nacl" {
   }
 
   tags = {
-    Name = "koda-web-nacl"
+    Name = "tcs-web-nacl"
   }
 }
 
 # Subnet association to nacl-web
 resource "aws_network_acl_association" "web-subnet-nacl" {
   network_acl_id = aws_network_acl.web_nacl.id
-  subnet_id      = aws_subnet.koda_subnet.id
+  subnet_id      = aws_subnet.tcs_subnet.id
 }
 
 # Create NACLs for app
 resource "aws_network_acl" "app_nacl" {
-  vpc_id = aws_vpc.koda_vpc.id
+  vpc_id = aws_vpc.tcs_vpc.id
 
   egress {
     protocol   = "tcp"
@@ -148,19 +148,19 @@ resource "aws_network_acl" "app_nacl" {
   }
 
   tags = {
-    Name = "koda-app-nacl"
+    Name = "tcs-app-nacl"
   }
 }
 
 # Subnet association to nacl-app
 resource "aws_network_acl_association" "app-subnet-nacl" {
   network_acl_id = aws_network_acl.app_nacl.id
-  subnet_id      = aws_subnet.koda_subnet_app.id
+  subnet_id      = aws_subnet.tcs_subnet_app.id
 }
 
 # Create NACLs for DB
 resource "aws_network_acl" "db_nacl" {
-  vpc_id = aws_vpc.koda_vpc.id
+  vpc_id = aws_vpc.tcs_vpc.id
 
   egress {
     protocol   = "tcp"
@@ -181,30 +181,30 @@ resource "aws_network_acl" "db_nacl" {
   }
 
   tags = {
-    Name = "koda-db-nacl"
+    Name = "tcs-db-nacl"
   }
 }
 
 # Subnet association to nacl-db
 resource "aws_network_acl_association" "db-subnet-nacl" {
   network_acl_id = aws_network_acl.db_nacl.id
-  subnet_id      = aws_subnet.koda_subnet_database.id
+  subnet_id      = aws_subnet.tcs_subnet_database.id
 }
 
 # Web security group rule
-resource "aws_security_group" "koda_sg_web" {
-  name        = "koda_sg_web"
+resource "aws_security_group" "tcs_sg_web" {
+  name        = "tcs_sg_web"
   description = "Allow SSH & HTTP traffic"
-  vpc_id      = aws_vpc.koda_vpc.id
+  vpc_id      = aws_vpc.tcs_vpc.id
 
   tags = {
-    Name = "koda-sg-web-firewall"
+    Name = "tcs-sg-web-firewall"
   }
 }
 
 # Web security group rule - SSH
-resource "aws_vpc_security_group_ingress_rule" "koda_sg_web_ssh" {
-  security_group_id = aws_security_group.koda_sg_web.id
+resource "aws_vpc_security_group_ingress_rule" "tcs_sg_web_ssh" {
+  security_group_id = aws_security_group.tcs_sg_web.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
@@ -212,8 +212,8 @@ resource "aws_vpc_security_group_ingress_rule" "koda_sg_web_ssh" {
 }
 
 # web security group rule - HTTP
-resource "aws_vpc_security_group_ingress_rule" "koda_sg_web_http" {
-  security_group_id = aws_security_group.koda_sg_web.id
+resource "aws_vpc_security_group_ingress_rule" "tcs_sg_web_http" {
+  security_group_id = aws_security_group.tcs_sg_web.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
@@ -221,19 +221,19 @@ resource "aws_vpc_security_group_ingress_rule" "koda_sg_web_http" {
 }
 
 # app security group rule
-resource "aws_security_group" "koda_app_sg" {
-  name        = "koda_app_sg"
+resource "aws_security_group" "tcs_app_sg" {
+  name        = "tcs_app_sg"
   description = "Allow SSH & 3000 traffic"
-  vpc_id      = aws_vpc.koda_vpc.id
+  vpc_id      = aws_vpc.tcs_vpc.id
 
   tags = {
-    Name = "koda-app-sg-firewall"
+    Name = "tcs-app-sg-firewall"
   }
 }
 
 # app security group rule - SSH
-resource "aws_vpc_security_group_ingress_rule" "koda_app_sg_ssh" {
-  security_group_id = aws_security_group.koda_app_sg.id
+resource "aws_vpc_security_group_ingress_rule" "tcs_app_sg_ssh" {
+  security_group_id = aws_security_group.tcs_app_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
@@ -241,8 +241,8 @@ resource "aws_vpc_security_group_ingress_rule" "koda_app_sg_ssh" {
 }
 
 # app security group rule - 3000
-resource "aws_vpc_security_group_ingress_rule" "koda_app_sg_3000" {
-  security_group_id = aws_security_group.koda_app_sg.id
+resource "aws_vpc_security_group_ingress_rule" "tcs_app_sg_3000" {
+  security_group_id = aws_security_group.tcs_app_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 3000
   ip_protocol       = "tcp"
@@ -250,19 +250,19 @@ resource "aws_vpc_security_group_ingress_rule" "koda_app_sg_3000" {
 }
 
 # db security group rule
-resource "aws_security_group" "koda_db_sg" {
-  name        = "koda_db_sg"
+resource "aws_security_group" "tcs_db_sg" {
+  name        = "tcs_db_sg"
   description = "Allow SSH & Mysql traffic"
-  vpc_id      = aws_vpc.koda_vpc.id
+  vpc_id      = aws_vpc.tcs_vpc.id
 
   tags = {
-    Name = "koda-db-sg-firewall"
+    Name = "tcs-db-sg-firewall"
   }
 }
 
 # db security group rule - SSH
-resource "aws_vpc_security_group_ingress_rule" "koda_db_sg_ssh" {
-  security_group_id = aws_security_group.koda_db_sg.id
+resource "aws_vpc_security_group_ingress_rule" "tcs_db_sg_ssh" {
+  security_group_id = aws_security_group.tcs_db_sg.id
   cidr_ipv4         = "10.0.0.0/16"
   from_port         = 22
   ip_protocol       = "tcp"
@@ -270,8 +270,8 @@ resource "aws_vpc_security_group_ingress_rule" "koda_db_sg_ssh" {
 }
 
 # db security group rule - Mysql
-resource "aws_vpc_security_group_ingress_rule" "koda_db_sg_mysql" {
-  security_group_id = aws_security_group.koda_db_sg.id
+resource "aws_vpc_security_group_ingress_rule" "tcs_db_sg_mysql" {
+  security_group_id = aws_security_group.tcs_db_sg.id
   cidr_ipv4         = "10.0.0.0/16"
   from_port         = 3306
   ip_protocol       = "tcp"
