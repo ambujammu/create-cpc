07b84486e8abbef51e45eb0d41726af67834adc5
ambujammu <ambujaweb123@gmail.com>
2024-07-20 13:44:19 +0530

    changes

--- virtualfile.tf
+++ virtualfile.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "koda_subnet" {
 # Creating Subnet for database server
 resource "aws_subnet" "koda_subnet_database" {
   vpc_id     = aws_vpc.koda_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-west-2a"
   map_public_ip_on_launch = "true"
 
