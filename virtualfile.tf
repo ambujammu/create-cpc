6204b96e6aab79e89e11715b877d1b5a6abcc943
ambujammu <ambujaweb123@gmail.com>
2024-07-20 13:33:18 +0530

    created id in subnet

--- virtualfile.tf
+++ virtualfile.tf
@@ -13,6 +13,7 @@ resource "aws_subnet" "koda_subnet" {
   vpc_id     = aws_vpc.koda_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-west-2a"
+  map_public_ip_on_lunch = "true"
 
   tags = {
     Name = "koda-subnet"
