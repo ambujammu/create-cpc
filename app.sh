18be1d0696a022f9e12db3b01f8985046d63c387
ambujammu <ambujaweb123@gmail.com>
2024-07-20 22:27:37 +0530

    added app and ec2

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt update -y
 apt install nginx -y
-rm -r /var/www/html*
+rm -r /var/www/html/*
 git clone https://github.com/ambujammu/create-cpc.git /var/www/html/
--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
-# ec2 instance
-resource "aws_instance" "koda_web_server" {
+# EC2 Instance
+resource "aws_instance" "tcs_web_server" {
   ami           = "ami-ami-0aff18ec83b712f05"
   instance_type = "t2.micro"
   key_name = "papu"
   subnet_id = aws_subnet.tcs_subnet.id
   vpc_security_group_ids = [aws_security_group.tcs_sg_web.id]
-  user_data = file("app.sh)
+  user_data = file("app.sh")
 
   tags = {
     Name = "HelloWorld"
